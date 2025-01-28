e9736fb4e30803691630ceff2de24ef0eb5fe759
Nick Morgan <nick@nickmorgan.dev>
2025-01-28 09:38:09 +0000

    fixed typo on test name

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,5 @@
 // This is an example test setup, this should pass.
-describe('110 ways tests', () => {
+describe('101 ways example test that always passes', () => {
     it('should work', () => {
         expect(1).toBe(1)
     })
